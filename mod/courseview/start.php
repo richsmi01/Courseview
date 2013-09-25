6748fcbf47e285ed7117b53a8d93d0d332933c56
richsmi01 <richsmi01@hotmail.com>
2013-09-24 22:34:40 -0400

    Original experiments...NOT COHESIVE!!!

--- mod/courseview/start.php
+++ mod/courseview/start.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Think about changing this from masters to courseview or something like that
+ * Write something profound here...
  */
 elgg_register_event_handler('init', 'system', 'courseviewInit');
 
@@ -17,32 +17,32 @@ function courseviewInit()
     
     //this allows us to hijack the sidebar.  Each time the sidebar is about to be rendered, this hook fires
     elgg_register_plugin_hook_handler('view', 'page/elements/sidebar', 'sidebar_intercept');
-    
-    //just to add a course to work with for testing
-    //masters_create_course(); 
-    
-    
-    
+       
     //register page event handler
     elgg_register_page_handler('courseview', 'courseviewPageHandler');
     
+    //set up our paths
     $plugin_path = elgg_get_plugins_path();
     $base_path = $plugin_path . 'courseview';
+    
+    //this is where I will put all of the action registrations for the forms
     elgg_register_action ("createcourse", $base_path.'/actions/courseview/createcourse.php');
     echo $CONFIG->actions;
 }
 
+//this is the method that gets called when one of the courseview urls is called.  We will use a switch to choose how to respond
 function courseviewPageHandler($page, $identifier)
 {
     $plugin_path = elgg_get_plugins_path();
     $base_path = $plugin_path . 'courseview/pages/courseview';
-    //echo 'richs guid '.uniqid();
+   
     set_input('rich', $page);
 
     switch ($page[0])  //switching on the first parameter passed through the RESTful url
     {
         case 'main':
             $status = ElggSession::offsetGet ('courseview');
+            //if the courseview session variable was false, toggle it to true and viceversa
             if ($status)
 //            {
 //                //put these back in order to exit courseview
@@ -51,15 +51,13 @@ function courseviewPageHandler($page, $identifier)
 //            }
 //            else
             {
-            ElggSession::offsetSet('courseview', true); //set session variable telling elgg that we are in 'masters' mode
-            require "$base_path/courseview.php"; //load the default courseview welcome page
+                ElggSession::offsetSet('courseview', true); //set session variable telling elgg that we are in 'masters' mode
+                require "$base_path/courseview.php"; //load the default courseview welcome page
             }
             break;
         case 'listview':
-            //echo '@@@'.$page[1];
             ElggSession::offsetSet('object_type', $page[1]);
             ElggSession::offsetSet('coursetreeindex',$page[2]);
-           // echo '###'.$page[1];
             require "$base_path/listview.php";
             break;
         case 'courseview':
@@ -67,7 +65,6 @@ function courseviewPageHandler($page, $identifier)
             require "$base_path/courseview.php";
             break;
          case 'managecourseview':
-            //set_input("object_type", 'all');
             require "$base_path/managecourseview.php";
             break;
          case 'addcourse':
@@ -77,10 +74,6 @@ function courseviewPageHandler($page, $identifier)
             ElggSession::offsetSet('courseview', false);
             forward('http://localhost/elgg/activity');
             break;
-//         case 'list':
-//            set_input("object_type", $page[1]);
-//            require "$base_path/view2.php";
-//            break;
         case 'managecourses':
             set_input("object_type", $page[1]);
             require "$base_path/managecourses.php";
@@ -106,31 +99,4 @@ function sidebar_intercept($hook, $entity_type, $returnvalue, $params)
 
 
 
-//just some cool snippets of code that I've discovered...add these to my onenote pages
-//$view = $params['view'];
-//    $testview = elgg_extract('view', $params);
-//    echo 'Rich was here!';
-//
-//    if ($params[vars][full_view] == 1)
-//    {
-//        $temp1 = elgg_get_context();
-//        //echo 'viewing:  '.$view;
-//        // elgg_extend_view($returnvalue, 'greetings');
-//        $returnview = elgg_view('blog/view') . elgg_view('masters/view2');
-//        // elgg_extend_view($returnvalue, 'greetings');
-//        // $vars = array('content' => $content,);
-//        // $sidebar = "";
-//// layout the page
-////$body = elgg_view_layout('one_sidebar', array(
-//        //'content' => $content,
-//        //'sidebar' => $sidebar
-////));
-//        //elgg_extend_view('page/elements/sidebar', 'masters/hijacksidebar');-----
-//        //
-//        //
-//        //       elgg_extend_view('page/elements/sidebar', 'masters/greetings'); 
-////        $entity = $params[vars][entity];
-////        $content = elgg_view_entity($entity);
-////        $sidebar =  elgg_echo(  "Hi Rich");
-////        $canvas_area = elgg_view_layout('one_sidebar', array('content' => $content, 'sidebar' => $sidebar));
-////        $returnvalue= $canvas_area;
+
