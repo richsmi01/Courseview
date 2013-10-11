2e31125ede41e0370f375666beb6ee5c1b89ff45
richsmi01 <richsmi01@hotmail.com>
2013-10-10 20:14:45 -0400

    Cleaning up code slightly for call tonight 2

--- mod/courseview/views/default/courseview/hijacksidebar.php
+++ mod/courseview/views/default/courseview/hijacksidebar.php
@@ -1,71 +1,38 @@
 <?php
 
-/*
- *   ::TODO:  This is a sucky way to do the CSS-- work through Matt's css tutorial plugin and fix this
- */
-
-
-
-
-
-//$coursetree = ElggSession::offsetGet('currentcourse');
-//echo elgg_echo('<br/>currently selected course is '. get_entity(599)->title.'<br/>');
-
-//echo elgg_echo("Current Course Guid:  ".$coursetree);
+$page = get_input('rich');
+//echo elgg_echo (var_dump($page));
+$cvcohort=$page[1];
+//echo elgg_echo ("Cohort: "+$cvcohort);
+//This pulls all menu entitities that have a relationship with this course...
 $unsortedmenu = elgg_get_entities_from_relationship(array
         ( 'relationship_guid' => get_entity(599)->guid,
             'relationship' => 'menu',
         )
      );
-//echo elgg_echo (var_dump($unsortedmenu));
+//Next, I have to sort the entities by the menuorder attribute so that I can display the course links in the correct order
+//TODO::Have no idea why this works!  Ask Matt what I've done
 $menu = array();
 foreach ($unsortedmenu as $key =>$row)
 {
     $menu[$key]=$row['menuorder'];
 }
 array_multisort($menu, SORT_ASC, $unsortedmenu);
-//echo elgg_echo (var_dump($menu));
-
-//$inventory = array(
-//
-//   array("type"=>"fruit", "price"=>3.50),
-//   array("type"=>"milk", "price"=>2.90),
-//   array("type"=>"pork", "price"=>5.43),
-//
-//);
-//
-//$price = array();
-//foreach ($inventory as $key => $row)
-//{
-//    $price[$key] = $row['price'];
-//}
-//array_multisort($price, SORT_DESC, $inventory);
-//echo elgg_echo (var_dump($price));
-
-//echo elgg_echo (var_dump($menu));
-//echo 'CourseTree'.var_dump($coursetree);
-//echo 'Courseview object guid'.var_dump(ElggSession::offsetGet('courseviewobject'));
-//echo elgg_echo(ElggSession::offsetGet('cvcourse'));
-
-//$menu= get_entity (ElggSession::offsetGet('courseviewobject'));
-//echo elgg_echo ('course info:  '.var_dump($course));
-//echo elgg_echo ('course tree'.var_dump ($course->coursetree));
-
-//Need to build the coursetree html with the appropriate class attributes so that the CSS can target it correctly and display
-//it in a tree.
-
-// ::TODO: the css needs to be fixed so that the open and close icons show up next to the folders and modules
 $menu=$unsortedmenu;
+
+//Here I am building the html of the treeview control and adding the correct css classes so that my css
+//can turn it into a tree that can be manipulated by the user 
 echo elgg_echo ('<div class ="css-treeview">');
   foreach ($menu as $temp)
 {
-    //echo elgg_echo('<br/>Getting menu item:  '.$temp->name);
+    //If this menu item should be indented from the previous one, add a <ul> tag to start a new unordered list
     if ($temp->indent==='+')
     {
         echo elgg_echo('
             <ul>
             ');
     }
+    //if this menu item should be outdented, close off our unordered list and list item
     if ($temp->indent==='-')
     {
         echo elgg_echo('
@@ -73,48 +40,18 @@ echo elgg_echo ('<div class ="css-treeview">');
             </li>
             ');
     }
-    //echo elgg_echo ('###'.$temp->menutype);
+    //if the menu item is a folder type, add a checkbox which the css will massage into the collapsing tree
     if ($temp->menutype=="folder")
     {
          echo elgg_echo('<ul>
            <li><input type ="checkbox"/><label><a> '.$temp->name.$temp->menuorder.'!!!</a></label>');
     }
+    //otherwise, let's just create a link to the contentpane and pass the guid of the menu object...the css class indent is also added here
  else
     {
-        echo elgg_echo("<li><a class = 'indent' href ='".  elgg_get_site_url()."courseview/contentpane/".$temp->guid."' >".$temp->name.$temp->menuorder."</a></li>");
+        echo elgg_echo("<li><a class = 'indent' href ='".  elgg_get_site_url()."courseview/contentpane/".$cvcohort."/".$temp->guid."' >".$temp->name.$temp->menuorder."</a></li>");
     }
 }     
-//for ($row=0; $row <sizeof ($coursetree); $row++)
-//{
-//    if ($coursetree[$row][indent ]== '+')
-//    {
-//        echo elgg_echo('
-//            <ul>
-//            ');
-//    }
-//   if ($coursetree[$row][indent ]== '-')
-//    {
-//        echo elgg_echo('
-//            </ul>
-//            </li>
-//            ');
-//    }
-//    if ($coursetree[$row][contenttype ]== 'folder'||$coursetree[$row][contenttype ]== 'module')
-//    {
-//        echo elgg_echo('<ul>
-//            <li><input type ="checkbox"/><label><a> '.$coursetree[$row][label].'</a></label>');
-//    }
-//    else if ($coursetree[$row][contenttype ]== 'folderOpen'||$coursetree[$row][contenttype ]== 'moduleOpen')
-//    {
-//        echo elgg_echo('<ul>
-//            <li><input type ="checkbox" checked ="checked"/><label><a> '.$coursetree[$row][label].'</a></label>');
-//    }
-// else 
-//    {
-//        echo elgg_echo('<li><a class = "indent" href = /elgg/courseview/contentpane/'.$coursetree [$row][subtype].'/'.$row.'>'.$coursetree[$row][label].'</a></li>');
-//    }
-//   
-//}
 echo elgg_echo ('</div>')  ;  
  
 ?>
